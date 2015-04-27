b2fbdfc2758c5430996a0e10f6ca0a4d2106b8c1
Sung Won Cho <mikeswcho@gmail.com>
2015-04-27 22:04:12 +1000

    Remove unnecessary files

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,6 @@ module Lifeklass
     config.autoload_paths += Dir["#{config.root}/lib/"]
     config.autoload_paths += Dir[Rails.root.join('app', 'presenters', '**/')]
 
-    config.assets.precompile += Ckeditor.assets
-
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
