9ae3d83eba836b148554798b69eb0aba811447a4
Sung Won Cho <mikeswcho@gmail.com>
2015-04-29 18:39:50 +1000

    Allow to become a mentor

--- engines/mentors/app/controllers/mentors/mentors_controller.rb
+++ engines/mentors/app/controllers/mentors/mentors_controller.rb
@@ -6,6 +6,18 @@ module Mentors
     def show
     end
 
+    def new
+      @mentor = Mentors::Mentor.new
+    end
+
+    def create
+      @mentor = current_user.build_mentor
+
+      if @mentor.save!
+        redirect_to mentors_app.mentor_path(@mentor)
+      end
+    end
+
     private
 
     def set_mentor
