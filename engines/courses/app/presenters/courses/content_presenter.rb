6ba647d783d013818325bf826847700fa2cc7aa0
Sung Won Cho <mikeswcho@gmail.com>
2015-05-09 18:14:46 +1000

    Conditionally display edit and delete buttons for contents

--- engines/courses/app/presenters/courses/content_presenter.rb
+++ engines/courses/app/presenters/courses/content_presenter.rb
@@ -4,5 +4,17 @@ module Courses
     def posted_on
       model.created_at.strftime('%m/%d/%Y')
     end
+
+    def edit_button
+      if h.current_user.is_mentor_of?(h.current_course)
+        h.link_to 'Edit Content', h.course_app.edit_course_menu_content_path(h.current_course, h.current_menu, model)
+      end
+    end
+
+    def delete_button
+      if h.current_user.is_mentor_of?(h.current_course)
+        h.link_to 'Delete Content', h.course_menu_content_path(h.current_course, h.current_menu, model), method: :delete, data: { confirm: 'Are you sure?' }
+      end
+    end
   end
 end
