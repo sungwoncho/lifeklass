35bb608abe9696dff43ef0aa03c922171a9d84a6
Sung Won Cho <mikeswcho@gmail.com>
2015-05-10 10:31:16 +1000

    Revamp registration page and configure devise strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def current_mentor
     return unless current_user
@@ -14,5 +15,18 @@ class ApplicationController < ActionController::Base
 
   def authenticate_mentor!
     current_mentor.present?
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up, &permitted_parameters)
+    devise_parameter_sanitizer.for(:account_update, &permitted_parameters)
+  end
+
+  private
+
+  def permitted_parameters
+    Proc.new { |u| u.permit(:email, :password, :password_confirmation, :current_password, :name) }
   end
 end
